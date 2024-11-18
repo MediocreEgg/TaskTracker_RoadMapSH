00c5f42fab41a16b8ecbba67de695336ae973852
MediocreEgg <topaciojohnbryant48@gmail.com>
2024-11-19 02:21:04 +0800

    MainFrame

---
+++ src/org/taskTracker/frontend/mainframe/Frame.java
@@ -0,0 +1,42 @@
+package org.taskTracker.frontend.mainframe;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Frame extends JFrame implements Iframe{
+
+	private static final long serialVersionUID = 3495824182818664026L;
+
+    /*
+     *
+     *  CONSTRUCTORS
+     *
+     */
+
+	public Frame(){
+        super();
+        this.setTitle(TITLE);
+        init();
+    }
+
+    /*
+     *
+     *
+     *
+     */
+
+	private void init(){
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+        this.setMinimumSize(new Dimension(FRAME_HEIGHT, FRAME_WIDTH));
+        this.setVisible(true);
+    }
+
+    /*
+     *
+     *
+     *
+     */
+	
+}
---
+++ src/org/taskTracker/frontend/mainframe/Iframe.java
@@ -0,0 +1,7 @@
+package org.taskTracker.frontend.mainframe;
+
+public interface Iframe {
+    String TITLE = "TaskPOJO Tracker";
+    int FRAME_HEIGHT = 320;
+    int FRAME_WIDTH = 180;
+}
